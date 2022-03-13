87c0ea47f50b840987e144cb96a05fe3e82d4031
Sammie Norris <norrissamantha@gmail.com>
2022-03-13 15:33:35 -0700

    Convert trigger to momentary button.
    Only run transport while the trigger is pressed.
    Keep throttle attached to transport.

--- src/main/cpp/TransportSubsystem.cpp
+++ src/main/cpp/TransportSubsystem.cpp
@@ -7,10 +7,10 @@ TransportSubsystem::TransportSubsystem(
         rev::CANSparkMax& TransportDrive,
         frc::Joystick& stick
 ) :
-    m_runTransport(false),
     m_buttonIndex(enableButtonIndex),
     m_transportDrive{TransportDrive},
-    m_stick{stick}
+    m_stick{stick},
+    m_encoder{m_transportDrive.GetEncoder()}
 {
   m_transportDrive.RestoreFactoryDefaults();
 }
@@ -18,86 +18,82 @@ TransportSubsystem::TransportSubsystem(
 
 void TransportSubsystem::ModeInit()
 {
-    // Call GetRawButtonPressed to discard any button presses
-    // made while the robot was disabled.
-    m_stick.GetRawButtonPressed(m_buttonIndex);
-    StopMotor();
+  // Call GetRawButtonPressed to discard any button presses
+  // made while the robot was disabled.
+  m_stick.GetRawButtonPressed(m_buttonIndex);
+  StopMotor();
 }
 
 // This is mostly copied from SPARK-MAX-Examples/C++/Get and Set Parameters
 void TransportSubsystem::RobotInit()
 {
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_transportDrive.RestoreFactoryDefaults();
+  /**
+   * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+   * in the SPARK MAX to their factory default state. If no argument is passed, these
+   * parameters will not persist between power cycles
+   */
+  m_transportDrive.RestoreFactoryDefaults();
 
-    /**
-     * Parameters can be set by calling the appropriate Set method on the CANSparkMax object
-     * whose properties you want to change
-     * 
-     * Set methods will return one of three REVLibError values which will let you know if the 
-     * parameter was successfully set:
-     *  REVLibError::kOk
-     *  REVLibError::kError
-     *  REVLibError::kTimeout
-     */
-    if(m_transportDrive.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast) != rev::REVLibError::kOk) {
-      frc::SmartDashboard::PutString("Transport Idle Mode", "Error");
-    }
+  /**
+   * Parameters can be set by calling the appropriate Set method on the CANSparkMax object
+   * whose properties you want to change
+   * 
+   * Set methods will return one of three REVLibError values which will let you know if the 
+   * parameter was successfully set:
+   *  REVLibError::kOk
+   *  REVLibError::kError
+   *  REVLibError::kTimeout
+   */
+  if(m_transportDrive.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast) != rev::REVLibError::kOk) {
+    frc::SmartDashboard::PutString("Transport Idle Mode", "Error");
+  }
 
-    /**
-     * Similarly, parameters will have a Get method which allows you to retrieve their values
-     * from the controller
-     */
-    if(m_transportDrive.GetIdleMode() == rev::CANSparkMax::IdleMode::kCoast) {
-      frc::SmartDashboard::PutString("Transport Idle Mode", "Coast");
-    } else {
-      frc::SmartDashboard::PutString("Transport Idle Mode", "Brake");
-    }
+  /**
+   * Similarly, parameters will have a Get method which allows you to retrieve their values
+   * from the controller
+   */
+  if(m_transportDrive.GetIdleMode() == rev::CANSparkMax::IdleMode::kCoast) {
+    frc::SmartDashboard::PutString("Transport Idle Mode", "Coast");
+  } else {
+    frc::SmartDashboard::PutString("Transport Idle Mode", "Brake");
+  }
 
-    // Set ramp rate to 0
-    if(m_transportDrive.SetOpenLoopRampRate(0) != rev::REVLibError::kOk) {
-      frc::SmartDashboard::PutString("Transport Ramp Rate", "Error");
-    }
+  // Set ramp rate to 0
+  if(m_transportDrive.SetOpenLoopRampRate(0) != rev::REVLibError::kOk) {
+    frc::SmartDashboard::PutString("Transport Ramp Rate", "Error");
+  }
 
-    // read back ramp rate value
-    frc::SmartDashboard::PutNumber("Transport Ramp Rate", m_transportDrive.GetOpenLoopRampRate());
+  // read back ramp rate value
+  frc::SmartDashboard::PutNumber("Transport Ramp Rate", m_transportDrive.GetOpenLoopRampRate());
 
-    // Display local member values.
-    frc::SmartDashboard::PutBoolean("Run Transport", m_runTransport);
+  // Display local member values.
+  frc::SmartDashboard::PutBoolean("Run Transport", false);
 }
 
 
 bool TransportSubsystem::RunPeriodic()
 {
-    // Toggle Transport state on button press.
-    if (m_stick.GetRawButtonPressed(m_buttonIndex))
-    {
-      m_runTransport = !m_runTransport;
-    }
-    // 
-    if (m_runTransport)
-    {
-    // Throttle is connected the slider on the controller.
-    // The throttle axis reads -1.0 when pressed forward.
-      m_transportDrive.Set(-m_stick.GetThrottle());
-    } else {
-      m_transportDrive.Set(0);
-    }
-    // periodically read voltage, temperature, and applied output and publish to SmartDashboard
-    frc::SmartDashboard::PutNumber("Transport Voltage", m_transportDrive.GetBusVoltage());
-    frc::SmartDashboard::PutNumber("Transport Temperature", m_transportDrive.GetMotorTemperature());
-    frc::SmartDashboard::PutNumber("Transport Output", m_transportDrive.GetAppliedOutput());
-    frc::SmartDashboard::PutBoolean("Run Transport", m_runTransport);
-    return m_runTransport;
+  // Toggle Transport state on button press.
+  bool runTransport = m_stick.GetRawButton(m_buttonIndex);
+  if (runTransport)
+  {
+  // Throttle is connected the slider on the controller.
+  // The throttle axis reads -1.0 when pressed forward.
+    m_transportDrive.Set(-m_stick.GetThrottle());
+  } else {
+    m_transportDrive.Set(0);
+  }
+  // periodically read voltage, temperature, and applied output and publish to SmartDashboard
+  frc::SmartDashboard::PutNumber("Transport Voltage", m_transportDrive.GetBusVoltage());
+  frc::SmartDashboard::PutNumber("Transport Temperature", m_transportDrive.GetMotorTemperature());
+  frc::SmartDashboard::PutNumber("Transport Output", m_transportDrive.GetAppliedOutput());
+  frc::SmartDashboard::PutBoolean("Run Transport", runTransport);
+  frc::SmartDashboard::PutNumber("Transport Throttle", m_stick.GetThrottle());
+  return runTransport;
 }
 
 
 void TransportSubsystem::StopMotor()
 {
-    m_runTransport = false;
-    m_transportDrive.StopMotor();
+  m_transportDrive.StopMotor();
 }
--- src/main/include/TransportSubsystem.h
+++ src/main/include/TransportSubsystem.h
@@ -24,21 +24,14 @@ public:
 
 public:
     // These are for tests an inspection.
-    bool isEnabled() const {return m_runTransport;}
     int buttonIndex() const {return m_buttonIndex;}
-    bool SetEnable(bool value)
-    {
-        m_runTransport = value;
-        return isEnabled();
-    }
 
 private:
-    bool m_runTransport;
     const int m_buttonIndex;
     // Non-owning reference to the motor controller.
     rev::CANSparkMax& m_transportDrive;
     // Non-owning reference to the joystick.
     frc::Joystick& m_stick;
     // Encoder object created to display velocity values
-    //rev::SparkMaxRelativeEncoder m_Transportencoder = m_TransportDrive.GetEncoder();
+    rev::SparkMaxRelativeEncoder m_encoder;
 };
